71edca4bb3acc85ac5231e0b2851b1d13f01b115
Isabela de Melo Silva <isabelansoc@gmail.com>
2023-01-30 17:34:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function mensagem (musica) {
+    console.log(`Olá, estou fazendo essa atividade e escutando a música ${musica} do musical Heathers.`)
+}
+mensagem("Meant to be yours")
+
+
+
+function meuNome (nome) {
+    console.log(`Olá, meu nome é ${nome} e estudo desenvolvimento front-end no Vai na Web.`)
+}
+meuNome("Isabela")
+
+
+
+let dados = function (nome, idade, estiloMusical) {
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical preferido é ${estiloMusical}.`)
+}
+dados("Isabela", "23", "pop")
+
+
+
+let midias = function (filme, musica) {
+    console.log(`O último filme que eu assisti foi ${filme} e a música que estou escutando agora é ${musica}.`)
+}
+midias("Nope", "Boneco Chuck D")
+
+
+
+function triplo (numero) {
+    return numero * 3
+}
+console.log(triplo(10))
+
+
+
+let anime = "Fullmetal"
+if (anime === "Fullmetal") {
+    console.log(`${anime} é o melhor anime do mundo.`)
+} else {
+    console.log(`${anime} não é o melhor anime do mundo.`)
+}
+
+
+
